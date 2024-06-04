b86bc93ac61d4c73e5e4f8470178747fc492a0d0
Emil Lal <38190245+emillal@users.noreply.github.com>
2024-06-04 11:02:21 +0530

    Add files via upload

---
+++ filelist_core.f
@@ -0,0 +1,11 @@
++define+S50 \
+verif/tb/tb_core.sv \
+verif/model/IS42VM16400K.v
+verif/model/mt48lc2m32b2.v \
+verif/model/mt48lc8m8a2.v  \
+rtl/core/sdrc_core.v \
+rtl/core/sdrc_bank_ctl.v \
+rtl/core/sdrc_bank_fsm.v \
+rtl/core/sdrc_bs_convert.v \
+rtl/core/sdrc_req_gen.v \
+rtl/core/sdrc_xfr_ctl.v 
